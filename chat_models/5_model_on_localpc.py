796dd42c5c1e6c5521c8123df7be35eceb327758
ziaulislam-mughal <ziaulislammughal0@gmail.com>
2026-03-08 09:01:12 +0500

    open souce model

--- chat_models/5_model_on_localpc.py
+++ chat_models/5_model_on_localpc.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 llm = HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.18-Chat-v1.0",
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
     pipeline_kwargs={"max_length": 2048, "temperature": 0.7, "top_p": 0.9},
 )
