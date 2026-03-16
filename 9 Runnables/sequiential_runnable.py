4693a2a4a2830efebd833131eb327d25b5592915
ziaulislam-mughal <ziaulislammughal0@gmail.com>
2026-03-16 16:00:56 +0500

    Runnables in langchain

---
+++ 9 Runnables/sequiential_runnable.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import Runnable
+
+#chain used with ootput parser . using chain we can make a proper pipline . 
+
+# 1. Load Environment
+load_dotenv()
+
+# 2. Check Token
+token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+if not token:
+    print("Error: HUGGINGFACEHUB_API_TOKEN is not set in the .env file.")
+
+# 3. Setup the Endpoint
+repo_id = "meta-llama/Llama-3.1-8B-Instruct"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7,
+    huggingfacehub_api_token=token,
+)
+
+model = ChatHuggingFace(llm=llm)
